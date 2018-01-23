7c4c1e473dad5158749061b9ac0a8b1fa7395af1
dsi <dsi@dsis-Mac.local>
2018-01-23 14:25:20 -0400

    Menu

---
+++ challengeApp/ViewControllerMenu.swift
@@ -0,0 +1,59 @@
+//
+//  ViewControllerMenu.swift
+//  challengeApp
+//
+//  Created by dsi on 23/01/2018.
+//  Copyright © 2018 etudiant1. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerMenu: UIViewController {
+    
+    var button = DropDownButton()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        button = DropDownButton.init(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        button.setTitle("Niveau", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(button)
+        
+        button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        button.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        
+        button.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        button.heightAnchor.constraint(lessThanOrEqualToConstant: 40).isActive = true
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+class DropDownButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.backgroundColor = UIColor.darkGray
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class DropDownView: UIView, UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+
+
+}
