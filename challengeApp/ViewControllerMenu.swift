809b168793b44b7ff98cc9eb7159c01c9e2e5606
dsi <dsi@dsis-Mac.local>
2018-01-23 15:13:58 -0400

    Menu

--- challengeApp/ViewControllerMenu.swift
+++ challengeApp/ViewControllerMenu.swift
@@ -36,10 +36,51 @@ class ViewControllerMenu: UIViewController {
 }
 
 class DropDownButton: UIButton {
+    
+    var DropView = DropDownView()
+    var height = NSLayoutConstraint()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         self.backgroundColor = UIColor.darkGray
+        
+        DropView = DropDownView.init(frame: CGRect.init(x: 0, y: 0, width: 0, height: 0))
+        DropView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    override func didMoveToSuperview() {
+        self.superview?.addSubview(DropView)
+        self.superview?.bringSubview(toFront: DropView)
+        DropView.topAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        DropView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        DropView.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
+        height = DropView.heightAnchor.constraint(equalToConstant: 0)
+    }
+    
+    var isOpen = false
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if isOpen == false {
+            
+            isOpen = true
+            
+            NSLayoutConstraint.deactivate([self.height])
+            self.height.constant = 150
+            NSLayoutConstraint.activate([self.height])
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: {
+                self.DropView.layoutIfNeeded()
+            }, completion: nil)
+            
+        }else{
+             isOpen = false
+            
+            NSLayoutConstraint.deactivate([self.height])
+            self.height.constant = 0
+            NSLayoutConstraint.activate([self.height])
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: {
+                self.DropView.layoutIfNeeded()
+            }, completion: nil)
+        }
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -48,12 +89,51 @@ class DropDownButton: UIButton {
 }
 
 class DropDownView: UIView, UITableViewDelegate, UITableViewDataSource {
+    
+    var DropDownOptions = [String]()
+    var tableView = UITableView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        tableView.backgroundColor = UIColor.darkGray
+        self.backgroundColor = UIColor.darkGray
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.addSubview(tableView)
+        
+        tableView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        tableView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return DropDownOptions.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        
+        cell.textLabel?.text = DropDownOptions[indexPath.row]
+        cell.backgroundColor = UIColor.darkGray
+        return cell
     }
 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(DropDownOptions[indexPath.row])
+    }
 
 }
+
